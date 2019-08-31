1df0cd4714b7339f870e85249056cec9df020113
Carlos Frias <carlosefrias@gmail.com>
2019-08-31 11:18:05 +0100

    update

--- helloworld.py
+++ helloworld.py
@@ -11,9 +11,9 @@ myriadProFont = pygame.font.SysFont("Myriad Pro", 48)
 hw = myriadProFont.render("Hello world!", 1, (255, 0, 255), (255, 255, 255))
 
 hw_size = hw.get_size()
-direction_x, direction_y = 1, 1
+# direction_x, direction_y = 1, 1
 
-x, y = 0, 0
+# x, y = 0, 0
 clock = pygame.time.Clock()
 while True:
     clock.tick(40)
@@ -23,9 +23,9 @@ while True:
     screen.fill((0,0,0))
     mouse_pos = pygame.mouse.get_pos()
     x, y = mouse_pos
-    if x + hw_size[0] > 800:
+    if x + hw_size[0] > window_size[0]:
         x = 800 - hw_size[0]
-    if y + hw_size[1] > 600:
+    if y + hw_size[1] > window_size[1]:
         y = 600 - hw_size[1]
     screen.blit(hw, (x,y))   
     pygame.display.update()
