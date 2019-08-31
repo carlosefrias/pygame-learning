f0e6a1d6964ef6d79e538ccdf8fa2ad6a732d619
Carlos Frias <carlosefrias@gmail.com>
2019-08-31 19:04:13 +0100

    Breakout game started

---
+++ Demo1/Game/Breakout.py
@@ -0,0 +1,45 @@
+from Shared import *
+class Breakout:
+    def __init__(self):
+        print(GameConstants.SCREEN_SIZE)
+        pass
+    
+    def start(self):
+        pass
+    
+    def changeScene(self, scene):
+        pass
+    
+    def getLevel(self):
+        pass
+    
+    def getScore(self):
+        pass
+    
+    def increaseScore(self, score):
+        pass
+    
+    def getLives(self):
+        pass
+    
+    def getBalls(self):
+        pass
+    
+    def getPad(self):
+        pass
+    
+    def playSound(self, soundClip):
+        pass
+    
+    def reduceLives(self):
+        pass
+    
+    def increaseLives(self):
+        pass
+    
+    def reset(self):
+        pass
+
+game = Breakout()
+
+game.start()
---
+++ Demo1/Game/Bricks/__init__.py
@@ -0,0 +1,1 @@
+from Bricks import Brick, LifeBrick, SpeedBrick
