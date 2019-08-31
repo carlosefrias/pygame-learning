b76dba96e989761154c90706ac6aa14e81e532e3
Carlos Frias <carlosefrias@gmail.com>
2019-08-31 10:29:21 +0100

    Bouncing hello world on the walls

--- helloworld.py
+++ helloworld.py
@@ -10,20 +10,23 @@ myriadProFont = pygame.font.SysFont("Myriad Pro", 48)
 
 hw = myriadProFont.render("Hello world!", 1, (255, 0, 255), (255, 255, 255))
 
+hw_size = hw.get_size()
+direction_x, direction_y = 1, 1
+
 x, y = 0, 0
 clock = pygame.time.Clock()
 while True:
-    clock.tick(200)
+    clock.tick(40)
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             sys.exit()
     screen.fill((0,0,0))
     screen.blit(hw, (x,y))
-    if(x <= 800):
-        x += 1
-    else:
-        x = 0
-        y += 50
-    if(y > 600):
-        x, y = 0, 0
+
+    x += 5 * direction_x
+    y += 5 * direction_y
+    if x + hw_size[0] > 800 or x <= 0:
+        direction_x *= -1
+    if y + hw_size[1] > 600 or y <= 0:
+        direction_y *= -1    
     pygame.display.update()
