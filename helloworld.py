6163cad4cfd2e94550dc54f45a76586b7cdbf86c
Carlos Frias <carlosefrias@gmail.com>
2019-08-31 10:16:47 +0100

    moving the hello world text

--- helloworld.py
+++ helloworld.py
@@ -10,9 +10,20 @@ myriadProFont = pygame.font.SysFont("Myriad Pro", 48)
 
 hw = myriadProFont.render("Hello world!", 1, (255, 0, 255), (255, 255, 255))
 
+x, y = 0, 0
+clock = pygame.time.Clock()
 while True:
+    clock.tick(200)
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             sys.exit()
-    screen.blit(hw, (0,0))
+    screen.fill((0,0,0))
+    screen.blit(hw, (x,y))
+    if(x <= 800):
+        x += 1
+    else:
+        x = 0
+        y += 50
+    if(y > 600):
+        x, y = 0, 0
     pygame.display.update()
